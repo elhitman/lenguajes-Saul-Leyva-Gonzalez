4e3253831533f7559b6c3a89411b8393d456b31b
elhitman <belmont9791@gmail.com>
2015-01-19 19:33:34 -0600

    Update LenguajesSaulLeyvaGonzalez.java

--- src/lenguajes/saul/leyva/gonzalez/LenguajesSaulLeyvaGonzalez.java
+++ src/lenguajes/saul/leyva/gonzalez/LenguajesSaulLeyvaGonzalez.java
@@ -16,7 +16,7 @@ public class LenguajesSaulLeyvaGonzalez {
      */
     public static void main(String[] args) {
         
-        System.out.println("mi primer programa de java II");
+        System.out.println("linea modificada");
     }
     
 }
